911f2258abb7532158ee9fefe278c46b99e48588
JENIL DHOKIYA <dhokiyajenil@gmail.com>
2026-01-30 11:10:56 +0530

    New MVC ASP.NET CORE project added

---
+++ CMS2026/Controllers/LoanController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS2026.Controllers
+{
+    public class LoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+
+        public IActionResult NewApplication()
+        {
+            return View();
+        }
+        public IActionResult UpdateApplication()
+        {
+            return View();
+        }
+        public IActionResult DiscardApplication()
+        {
+            return View();
+        }
+    }
+}
